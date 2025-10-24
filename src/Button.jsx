84ed658f114fe19a7b7763e4b20c0b48c7eb3720
ammmar7874-dev <ammmar7874@gmail.com>
2025-10-24 15:54:52 +0500

    react 1st lecture

---
+++ src/Button.jsx
@@ -0,0 +1,14 @@
+const Button = () => {
+  let name = "abc";
+  let age = 20;
+  let numbers = [1, 2, 3, 4, 5];
+  return (
+    <>
+      <h1>
+        my name is {name} and my age is {age}
+      </h1>
+      <button>click</button>
+    </>
+  );
+};
+export default Button;
